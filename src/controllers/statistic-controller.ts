e5331530e25acbed27a80132e8f3ba74ac8733b8
gsidnick <gsidnick@gmail.com>
2023-02-05 12:49:38 +0300

    feat: add statistic controller

---
+++ src/controllers/statistic-controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import Statistic from '../models/statistic';
+
+export function getStatistics(req: Request, res: Response) {
+  Statistic.find()
+    .then((statistic) => res.status(200).json(statistic))
+    .catch((error) => res.status(500).send(error.message));
+}
+
+export function getStatistic(req: Request, res: Response) {
+  const { id } = req.params;
+  Statistic.findById(id)
+    .then((statistic) => res.status(200).json(statistic))
+    .catch((error) => res.status(500).send(error.message));
+}
+
+export function addStatistic(req: Request, res: Response) {
+  const { score, level } = req.body;
+  const newStatistic = new Statistic({ score, level });
+  newStatistic
+    .save()
+    .then((statistic) => res.status(201).json(statistic))
+    .catch((error) => res.status(500).send(error.message));
+}
+
+export function editStatistic(req: Request, res: Response) {
+  const { score, level } = req.body;
+  const { id } = req.params;
+  Statistic.findByIdAndUpdate(id, { score, level }, { new: true })
+    .then((statistic) => res.status(200).json(statistic))
+    .catch((error) => res.status(500).send(error.message));
+}
+
+export function deleteStatistic(req: Request, res: Response) {
+  const { id } = req.params;
+  Statistic.findByIdAndDelete(id)
+    .then(() => res.status(200).json(id))
+    .catch((error) => res.status(500).send(error.message));
+}
