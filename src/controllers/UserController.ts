ec25a669fe1b7ce64da609367fbaaeff6892cc32
gsidnick <gsidnick@gmail.com>
2023-02-06 21:08:17 +0300

    feat: add user controller

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+
+class UserController {
+  async registration(req: Request, res: Response) {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async login(req: Request, res: Response) {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async logout(req: Request, res: Response) {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async activate(req: Request, res: Response) {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async refresh(req: Request, res: Response) {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+export default new UserController();
