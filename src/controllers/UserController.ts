277b07f0997d3a83bdf8579d7f6645343a0890d8
gsidnick <gsidnick@gmail.com>
2023-02-06 23:02:17 +0300

    feat: add user controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from 'express';
+import IUserData from '../interfaces/IUserData';
+import IUserCredential from '../interfaces/IUserCredential';
+import userService from '../services/UserService';
 
 class UserController {
   async registration(req: Request, res: Response) {
     try {
+      const { email, password }: IUserCredential = req.body;
+      const userData: IUserData = await userService.registration(email, password);
+      res.cookie('refreshToken', userData.refreshToken, { maxAge: 864000000, httpOnly: true });
+      return res.json(userData);
     } catch (error) {
-      console.error(error);
+      if (error instanceof Error) {
+        res.status(500).send(error.message);
+      }
     }
   }
 
